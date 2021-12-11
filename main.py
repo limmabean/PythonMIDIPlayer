1f8f43abb3dd6d3054b3cc287fa32484429be765
limmabean <mlim45@gatech.edu>
2021-12-11 15:46:48 -0500

    Added a randomizer
    Also put zmq into the requirements.txt

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import mido
 import zmq
 import threading
 from mido import MidiFile
+import glob, os
+
+random.seed(None)
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 counter = 0
 timer = 0
-
+drum_files = []
+note_files = []
+drum_mid = []
+note_mid = []
+random_midi_index = 0
 
 def unity_server():
     while True:
@@ -29,41 +36,57 @@ def unity_server():
 
 
 def midi_out(port_name):
+    global random_midi_index
     # Import track details
     with mido.open_output(port_name, virtual=True) as outport:
-        mid = []
-        if (port_name=="NOTES"):
-            mid = MidiFile('midi/notes/generated.mid', clip=True)
-        else:
-            mid = MidiFile('midi/drums/2021-12-07_041432_01.mid', clip=True)
         while True:
-            for msg in mid.play():
-                outport.send(msg)
+            if port_name == "NOTES":
+                print("Playing file: " + note_files[random_midi_index % len(note_files)])
+                for msg in note_mid[random_midi_index % len(note_mid)].play():
+                    outport.send(msg)
+                random_midi_index = random.randint(0, 50)
+            elif port_name == "DRUMS":
+                print("Playing file: " + drum_files[random_midi_index % len(drum_files)])
+                for msg in drum_mid[random_midi_index % len(drum_mid)].play():
+                    outport.send(msg)
+                random_midi_index = random.randint(0, 50)
+
 
 
 class myThread(threading.Thread):
-    def __init__(self, threadID, name, counter, type):
+    def __init__(self, thread_id, name, thread_type):
         threading.Thread.__init__(self)
-        self.threadID = threadID
+        self.thread_id = thread_id
         self.name = name
-        self.counter = counter
-        self.type = type
+        self.thread_type = thread_type
 
     def run(self):
-        print("Starting " + self.name + "\n")
-        if self.type == "MIDI":
+        print("Starting " + self.name + " thread" + "\n")
+        if self.thread_type == "MIDI":
             midi_out(self.name)
-        elif self.type == "SERVER":
+        elif self.thread_type == "SERVER":
             unity_server()
         print("Exiting " + self.name + "\n")
 
 
 if __name__ == '__main__':
-    thread1 = myThread(1, "NOTES", 1, "MIDI")
-    thread2 = myThread(2, "DRUMS", 2, "MIDI")
-    thread3 = myThread(3, "UNITY_SERVER", 3, "SERVER")
+    print("Creating threads...")
+    notes_thread = myThread(1, "NOTES", "MIDI")
+    drums_thread = myThread(2, "DRUMS", "MIDI")
+    unity_server_thread = myThread(3, "UNITY_SERVER", "SERVER")
 
-    thread1.start()
-    thread2.start()
-    thread3.start()
+    print("Loading midi files...")
+    for file in glob.glob("./midi/drums/*.mid"):
+        drum_files.append(file)
+    for file in glob.glob("./midi/notes/*.mid"):
+        note_files.append(file)
 
+    for file in drum_files:
+        drum_mid.append(MidiFile(file, clip=True))
+    for file in note_files:
+        note_mid.append(MidiFile(file, clip=True))
+    print("Midi files loaded.")
+
+    notes_thread.start()
+    drums_thread.start()
+    unity_server_thread.start()
